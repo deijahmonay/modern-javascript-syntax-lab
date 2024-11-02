5d5755e37208c6c44cf0a13d29f462a0bc095468
deijahmonay <deijah@shack15.com>
2024-11-02 15:42:24 -0700

    complete exercise 1

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 // Create a new array where each value is multiplied by 2 and log the new array.
 
 // Your code here
+
+const nums2 = nums.map((currentElement) => {
+  return currentElement * 2;
+});
+
+console.log(nums2)
